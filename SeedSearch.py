5575a3d993c412fe8102b12efb148545fb44e91c
pentolope <pentolope@gmail.com>
2021-01-23 17:10:15 -0700

    Update SeedSearch.py

--- SeedSearch.py
+++ SeedSearch.py
@@ -2,8 +2,8 @@ import os
 import subprocess
 from glob import glob
 sep="\\"
-seed_lower=1
-seed_upper=20
+seed_lower=17
+seed_upper=19
 quartus_dir="H:\\FPGA\\program\\quartus\\bin64\\"
 
 def directory_find(origin):
@@ -95,7 +95,39 @@ if "set_global_assignment -name SEED 1" in qsf_content:
 		f.close()
 	for seed_value in range(seed_lower,seed_upper+1):
 		print('Executing seed '+str(seed_value))
-		subprocess.call("start /low /wait /b seed_search"+sep+"run_script_"+str(seed_value)+".bat",shell=True)
+		subprocess.call('start /low /wait /b seed_search'+sep+'run_script_'+str(seed_value)+'.bat',shell=True) # can't have quotes for this command... idk why, but it breaks things
+		subprocess.call('del seed_search'+sep+'run_script_'+str(seed_value)+'.bat',shell=True)                 # can't have quotes for this command... idk why, but it breaks things
+	print('Collecting results...')
+	final_slack_seed_values=[]
+	for seed_value in range(seed_lower,seed_upper+1):
+		temp_slack_values=[]
+		f=open("seed_search"+sep+"cookie_"+str(seed_value)+".sta.summary","r")
+		fcl=f.read().split('\n')
+		f.close()
+		for line in fcl:
+			line_split=line.split(":")
+			if len(line_split)==2 and line_split[0]=="Slack ":
+				temp_slack_values.append(float(line_split[1]))
+		if len(temp_slack_values)!=0:
+			final_slack_seed_values.append((min(temp_slack_values),seed_value))
+		else:
+			print("Warning: Could not find slack data for seed "+str(seed_value))
+	final_slack_seed_values=sorted(final_slack_seed_values,key=lambda x:x[0])
+	result_file_content="Seed : Slack\n"
+	if len(final_slack_seed_values)!=0:
+		print("Results below, sorted by lowest slack first.")
+		print("")
+		print("Seed : Slack")
+		for slack_value,seed_value in final_slack_seed_values:
+			seed_value_s=str(seed_value)
+			while len(seed_value_s)<5:seed_value_s=seed_value_s+" "
+			print(seed_value_s+" : "+str(slack_value))
+			result_file_content=result_file_content+seed_value_s+" : "+str(slack_value)+"\n"
+		f=open("seed_search"+sep+"results.txt","w")
+		f.write(result_file_content)
+		f.close()
+	else:
+		print("No slack data found!")
 else:
 	print("you must set the set the seed value in the .qsf to 1 before running this script")
 
