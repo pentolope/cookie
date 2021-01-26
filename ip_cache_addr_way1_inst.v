4cfd4b7e825e63293e0c0909d1b90299d4af5c3a
pentolope <pentolope@gmail.com>
2021-01-25 17:32:46 -0700

    remove some unneeded files
    these were some autogenerated files that are not needed

--- ip_cache_addr_way1_inst.v
+++
@@ -1,8 +0,0 @@
-ip_cache_addr_way1	ip_cache_addr_way1_inst (
-	.clock ( clock_sig ),
-	.data ( data_sig ),
-	.rdaddress ( rdaddress_sig ),
-	.wraddress ( wraddress_sig ),
-	.wren ( wren_sig ),
-	.q ( q_sig )
-	);
--- ip_cache_addr_way3_inst.v
+++
@@ -1,8 +0,0 @@
-ip_cache_addr_way3	ip_cache_addr_way3_inst (
-	.clock ( clock_sig ),
-	.data ( data_sig ),
-	.rdaddress ( rdaddress_sig ),
-	.wraddress ( wraddress_sig ),
-	.wren ( wren_sig ),
-	.q ( q_sig )
-	);
